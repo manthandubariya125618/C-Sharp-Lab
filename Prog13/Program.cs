2e9cf0ee40eaf6010f0d80470821a3eb7594a7e6
manthandubariya125618 <manthan.dubariya125618@marwadiuniversity.ac.in>
2026-04-06 16:31:56 +0530

    Add files via upload

---
+++ Prog13/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+    Definition 13
+    Write a program to find minimum of three numbers using conditional operator
+*/
+
+using System;
+
+namespace Prog13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter three numbers to find the minimum:");
+
+            Console.Write("Number 1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Number 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Number 3: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int min = (num1 < num2) ? ((num1 < num3) ? num1 : num3) : ((num2 < num3) ? num2 : num3);
+
+            Console.WriteLine("The minimum of the three numbers is: " + min);
+        }
+    }
+}
