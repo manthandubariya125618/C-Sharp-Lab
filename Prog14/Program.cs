b54c8263a462cc58dfe68f38cac06fcbb956b03b
manthandubariya125618 <manthan.dubariya125618@marwadiuniversity.ac.in>
2026-04-06 16:32:31 +0530

    Add files via upload

---
+++ Prog14/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+    Definition 14
+    Write a program to check weather a number is palindrome or not 
+    e.g. input: 121, output: It is palindrome 
+         input:124, output: It is not palindrome
+*/
+
+using System;
+namespace Prog14
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int originalNumber = number;
+            int reversedNumber = 0;
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                reversedNumber = (reversedNumber * 10) + digit;
+                number /= 10;
+            }
+
+            if (originalNumber == reversedNumber)
+            {
+                Console.WriteLine(originalNumber + " is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine(originalNumber + " is not a palindrome.");
+            }
+        }
+    }
+}
