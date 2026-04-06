438acf7a1aa9964057828a094deb68a7f58d2e14
manthandubariya125618 <manthan.dubariya125618@marwadiuniversity.ac.in>
2026-04-06 16:34:07 +0530

    Add files via upload

---
+++ Prog16/Program.cs
@@ -0,0 +1,41 @@
+﻿
+/*
+    Definition 16
+    Write a program to check whether a number is Armstrong or not 
+    e.g. Input:153 output: It is Armstrong 
+    e.g. Input:100 output: It is not Armstrong  
+*/
+
+using System;
+namespace Prog16
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int num, sum = 0, temp, remainder;
+
+            Console.Write("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            temp = num;
+
+            while (temp != 0)
+            {
+                remainder = temp % 10;
+                sum += remainder * remainder * remainder;
+                temp /= 10;
+            }
+
+            if (sum == num)
+            {
+                Console.WriteLine(num + " is an Armstrong number.");
+            }
+            else
+            {
+                Console.WriteLine(num + " is not an Armstrong number.");
+            }
+        }
+    }
+}
