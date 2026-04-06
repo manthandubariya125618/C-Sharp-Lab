a5e5b6536ce46338cbee759e8bcbbd5ec676a272
manthandubariya125618 <manthan.dubariya125618@marwadiuniversity.ac.in>
2026-04-06 16:23:09 +0530

    Add files via upload

---
+++ Prog04/Program.cs
@@ -0,0 +1,30 @@
+﻿
+/*
+    Definition 4
+    Write a program to input principal amount, rate and year and display the compound interest.
+*/
+
+using System;
+
+namespace Prog04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter principal amount: ");
+            double principal = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter rate of interest (in %): ");
+            double rate = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter time period (in years): ");
+            double time = double.Parse(Console.ReadLine());
+
+            double compoundInterest = principal * Math.Pow((1 + rate / 100), time) - principal;
+
+            Console.WriteLine("Compound Interest: " + compoundInterest);
+        }
+    }
+}
