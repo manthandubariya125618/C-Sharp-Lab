8a240c278f3443d9ec82357ca50acb03e2f55ff4
manthandubariya125618 <manthan.dubariya125618@marwadiuniversity.ac.in>
2026-04-06 16:30:42 +0530

    Add files via upload

---
+++ Prog11/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+    Definition 11
+    Write a program to find all prime numbers between two values inputted by the user.
+*/
+
+using System;
+
+namespace Prog11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Prime numbers between " + num1 + " and " + num2 + ":");
+
+            for (int i = num1; i <= num2; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
