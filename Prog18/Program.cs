f94ad6356d1f6ea34f6d5479c4552bc527b47559
manthandubariya125618 <manthan.dubariya125618@marwadiuniversity.ac.in>
2026-04-06 16:34:44 +0530

    Add files via upload

---
+++ Prog18/Program.cs
@@ -0,0 +1,38 @@
+﻿
+/*
+    Definition 18
+    Write a program to display maximum number from 5 
+    numbers inputted by user by using the concept of arrays   
+*/
+
+using System;
+
+namespace Prog18
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int[] numbers = new int[5];
+            Console.WriteLine("Enter 5 numbers:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int max = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+
+            Console.WriteLine("The maximum number is: " + max);
+        }
+    }
+}
